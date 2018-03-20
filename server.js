20bc3a04923420a678609ee13e59ba7744f34c3b
Caitlin Bales (MSFT) <caitbal@microsoft.com>
2018-03-20 13:46:29 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+const app = express();
+
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static(__dirname + "/src"));
+
+const port = process.env.PORT || 3001;
+
+app.get('/', (req,res) => {
+return res.end('Api working');
+});
+
+    app.listen(port,() => {
+    console.log(`App Server Listening at ${port}`);
+    });
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { WeddingComponent } from './wedding/wedding.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeddingComponent,
+    ScheduleComponent,
+    GalleryComponent,
+    NavigationComponent,
+    RsvpComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
