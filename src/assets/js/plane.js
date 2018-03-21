33aa34894e5bfc26497cf606a5e8475b2b37a3fb
Caitlin Bales (MSFT) <caitbal@microsoft.com>
2018-03-21 16:22:13 -0700

    Add RSVP section

---
+++ src/assets/js/plane.js
@@ -0,0 +1,21 @@
+$(document).on("click", "#send", function(){
+      setTimeout(function() {
+          $('#plate').removeClass('front');
+          $('#container').removeClass('beginning');
+          $('.curvable').addClass('curved');
+          setTimeout(function() {
+              $('#container').addClass('hover');
+              setTimeout(function() {
+                  $('#container').addClass('fly_away_first');
+                  setTimeout(function() {
+                      $('#container').addClass('fly_away');
+                      setTimeout(function(){
+                          $('#plate').addClass('front');
+                          $('#container').removeClass('fly_away fly_away_first hover').addClass('beginning');
+                          $('.curvable').removeClass('curved');
+                      },3000);
+                  }, 600);
+              }, 1200);
+          }, 1500);
+      }, 800);
+  });
