faa292f77e4bee5976d5f3fe909369e5e315dc53
Caitlin Bales <c.bales@outlook.com>
2018-09-25 22:27:56 -0700

    Some rudimentary error handling

--- src/assets/js/plane.js
+++ src/assets/js/plane.js
@@ -57,54 +57,68 @@ $(document).on("click", "#send", function(){
       
     $.post("/lookupUser", user)
         .success(function(res) {
-            console.log(res.rsvpData);
-            var innerHtml = '<h2 style="color: inherit">Your invitation</h2>';
-            if (res.rsvpData.length > 0) {
-                innerHtml += "<p>You have already RSVP'd. Thanks!</p>";
-                innerHtml += "<p>If you need to change your RSVP, please send us an <a href='mailto:c.bales@outlook.com'>email</a>.</p>";
+            if (res.party.length == 0) {
+                var innerHtml = "<p>Sorry, we're having trouble finding your digital invite. Try entering your name exactly as it appears on your paper invitation.</p>";
+                innerHtml += "<p>If you are still having trouble, feel free to <a href='mailto:c.bales@outlook.com'>email us</a> your RSVP directly.";
+                $('.invitees').html(innerHtml);
+                $('.invitees').slideDown('400');
+                $("#lookup-searching").hide();
+                $('#lookup-button').show();
+            } else {
+                var innerHtml = '<h2 style="color: inherit">Your invitation</h2>';
+                if (res.rsvpData.length > 0) {
+                    innerHtml += "<p>You have already RSVP'd. Thanks!</p>";
+                    innerHtml += "<p>If you need to change your RSVP, please send us an <a href='mailto:c.bales@outlook.com'>email</a>.</p>";
+                }
+                
+                res.party.forEach(guest => {
+                    innerHtml += '<div class="invitation-guest"><input type="text" hidden class="firstname" value='+guest[0]+'>';
+                    innerHtml += '<input type="text" hidden class="lastname" value='+guest[1]+'>';
+                    innerHtml += '<h3 class="guestname" style="text-align: left; font-size: 1.2rem;">' + guest[0] + ' ' + guest[1] + '</h3>';
+                    innerHtml += ' <label style="margin-right: 20px"><input class="uk-radio" type="radio" name="rsvp-'+guest[0]+'" id="rsvp-yes-'+guest[0]+'" value="yes"> I will be attending</label>  ';
+                    innerHtml += '<label><input class="uk-radio" type="radio" name="rsvp-'+guest[0]+'" id="rsvp-no-'+guest[0]+'" value="no"> I will not be attending</label>';
+                    innerHtml += '<p class="diet-link" id="diet-link-'+guest[0]+'" style="text-align: left; cursor: pointer">+ Dietary restrictions</p>'
+                    innerHtml += '<input class="uk-input diet-input" id="diet-'+guest[0]+'" type="text" style="margin-top: 10px; display: none" placeholder="Dietary restrictions" />';
+                    innerHtml += '</div>';
+                });
+                innerHtml += '<hr />';
+                innerHtml += '<br/><br/><input class="uk-input" id="song-request" type="text" placeholder="Favorite dance song" />';
+                $('.invitees').html(innerHtml);
+                $("#lookup-searching").hide();
+                $('#lookup-button').show();
+                $('.invitees').slideDown('400');
+                $("#send").show();
+
+                if (res.rsvpData.length > 0) {
+                    var form = document.getElementById("rsvp-form");
+                    var elements = form.elements;
+                    for (var i = 0, len = elements.length; i < len; ++i) {
+                        elements[i].readOnly = true;
+                    }
+                    document.getElementById("firstName").readOnly = false;
+                    document.getElementById("lastName").readOnly = false;
+                    $("#rsvp-form input[type=radio]").attr('disabled', true);
+                    $("#send").hide();
+
+                    res.rsvpData.forEach(guest => {
+                        if (guest[2] == "yes") {
+                            $("#rsvp-yes-"+guest[0]).prop("checked", true);
+                        } else if (guest[2] == "no") {
+                            $("#rsvp-no-"+guest[0]).prop("checked", true);
+                        }
+                    });
+                    if (res.rsvpData[0][4]) {
+                        $("#song-request").val(res.rsvpData[0][4]);
+                    }
+                }
             }
-            //$(".invitee").show();
-            
-            res.party.forEach(guest => {
-                innerHtml += '<div class="invitation-guest"><input type="text" hidden class="firstname" value='+guest[0]+'>';
-                innerHtml += '<input type="text" hidden class="lastname" value='+guest[1]+'>';
-                innerHtml += '<h3 class="guestname" style="text-align: left; font-size: 1.2rem;">' + guest[0] + ' ' + guest[1] + '</h3>';
-                innerHtml += ' <label style="margin-right: 20px"><input class="uk-radio" type="radio" name="rsvp-'+guest[0]+'" id="rsvp-yes-'+guest[0]+'" value="yes"> I will be attending</label>  ';
-                innerHtml += '<label><input class="uk-radio" type="radio" name="rsvp-'+guest[0]+'" id="rsvp-no-'+guest[0]+'" value="no"> I will not be attending</label>';
-                innerHtml += '<p class="diet-link" id="diet-link-'+guest[0]+'" style="text-align: left; cursor: pointer">+ Dietary restrictions</p>'
-                innerHtml += '<input class="uk-input diet-input" id="diet-'+guest[0]+'" type="text" style="margin-top: 10px; display: none" placeholder="Dietary restrictions" />';
-                innerHtml += '</div>';
-            });
-            innerHtml += '<hr />';
-            innerHtml += '<br/><br/><input class="uk-input" id="song-request" type="text" placeholder="Favorite dance song" />';
+        }).error(function(err){
+            var innerHtml = "<p>Something went wrong!</p>";
+            innerHtml += "<p>It's not you, it's us. Please send us an <a href='mailto:c.bales@outlook.com'>email</a> and we'll work on getting it fixed for you.";
             $('.invitees').html(innerHtml);
+            $('.invitees').slideDown('400');
             $("#lookup-searching").hide();
             $('#lookup-button').show();
-            $('.invitees').slideDown('400');
-            $("#send").show();
-
-            if (res.rsvpData.length > 0) {
-                var form = document.getElementById("rsvp-form");
-                var elements = form.elements;
-                for (var i = 0, len = elements.length; i < len; ++i) {
-                    elements[i].readOnly = true;
-                }
-                document.getElementById("firstName").readOnly = false;
-                document.getElementById("lastName").readOnly = false;
-                $("#rsvp-form input[type=radio]").attr('disabled', true);
-                $("#send").hide();
-
-                res.rsvpData.forEach(guest => {
-                    if (guest[2] == "yes") {
-                        $("#rsvp-yes-"+guest[0]).prop("checked", true);
-                    } else if (guest[2] == "no") {
-                        $("#rsvp-no-"+guest[0]).prop("checked", true);
-                    }
-                });
-                if (res.rsvpData[0][4]) {
-                    $("#song-request").val(res.rsvpData[0][4]);
-                }
-            }
         });
   });
 
