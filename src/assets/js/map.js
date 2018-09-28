5eabfe7e504211fa9e9ab2f5d43d869f1c82e557
Caitlin Bales <c.bales@outlook.com>
2018-09-27 19:27:09 -0700

    I'm the map I'm the map I'm the map I'm the map... I'm the map!

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -6,15 +6,57 @@ $(document).on("click", "#reception-map-button", function(){
 
         if (result.device.type == "mobile") {
             if (result.device.vendor == "Apple")
-                window.open("maps://maps.google.com/maps?q=Chez%20Hay&amp;ll=");
+                window.open("maps://maps.google.com/maps?q=Roca%20Ridge&amp;ll=");
             else 
-                window.open("https://maps.google.com/maps?q=Chez%20Hay&amp;ll=");
+                window.open("https://maps.google.com/maps?q=Roca%20Ridge&amp;ll=");
         }
         else {
             $("#reception-map").show();
         }
     } else {
         $("#reception-map").hide();
+    }
+    
+});
+
+$(document).on("click", "#residence-inn-map-button", function(){
+    if ($("#residence-inn-map").is(":visible") == false) {
+        var parser = new UAParser();
+        var result = parser.getResult();
+        console.log(result);
+
+        if (result.device.type == "mobile") {
+            if (result.device.vendor == "Apple")
+                window.open("maps://maps.google.com/maps?q=Residence%20Inn%20Lincoln%20South&amp;ll=");
+            else 
+                window.open("https://maps.google.com/maps?q=Residence%20Inn%20Lincoln%20South&amp;ll=");
+        }
+        else {
+            $("#residence-inn-map").show();
+        }
+    } else {
+        $("#residence-inn-map").hide();
+    }
+    
+});
+
+$(document).on("click", "#holiday-inn-map-button", function(){
+    if ($("#holiday-inn-map").is(":visible") == false) {
+        var parser = new UAParser();
+        var result = parser.getResult();
+        console.log(result);
+
+        if (result.device.type == "mobile") {
+            if (result.device.vendor == "Apple")
+                window.open("maps://maps.google.com/maps?q=Holiday%20Inn%20Lincoln%20Southwest&amp;ll=");
+            else 
+                window.open("https://maps.google.com/maps?q=Holiday%20Inn%20Lincoln%20Southwest&amp;ll=");
+        }
+        else {
+            $("#holiday-inn-map").show();
+        }
+    } else {
+        $("#holiday-inn-map").hide();
     }
     
 });
